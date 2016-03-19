6eb50f60f54419a34738b95dc3484ffe79b05452
davidpuchaicela96 <josepth1103@hotmail.com>
2016-03-19 08:07:32 -0500

    Creacion del proyecto y de la clse Main

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author jose
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Bienvenido");
+    }
+    
+}
