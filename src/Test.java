92b0039b155869cbcee9bdbaaf745ff1e2f00406
latinoamericano <acuariocel@gmail.com>
2016-03-19 08:48:45 -0500

    Correcciones realizadas Ing. Diego

--- src/Test.java
+++ src/Test.java
@@ -1,15 +1,16 @@
 
 import javax.swing.JOptionPane;
 
+
+
 /**
  *
  * @author José
- * @fecha 19-03-2016
+ * @fecha 19-03-2016F
  */
-public class Main {
+public class Test {
     
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Bienvenid@");
+        JOptionPane.showMessageDialog(null,"Test");
     }
-    
 }
